a114bd52428c6d03796a6918c83b0f4ad4405fb0
AkaneOno <a-ono@winas.jp>
2020-06-02 13:31:18 +0900

    edit

--- routes/web.php
+++ routes/web.php
@@ -20,30 +20,32 @@ Route::get('/', function () {
 
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+  //管理者ニュースポストに関して
   Route::get('news/create', 'Admin\NewsController@add');
   Route::post('news/create', 'Admin\NewsController@create');
 
   Route::get('news/edit', 'Admin\NewsController@edit');
   Route::post('news/edit', 'Admin\NewsController@update');
 
+  Route::get('news', 'Admin\NewsController@index');
   Route::get('news/delete', 'Admin\NewsController@delete');
 
+  //管理者プロフィールに関して
   Route::get('profile/create', 'Admin\ProfileController@add');
   Route::post('profile/create', 'Admin\ProfileController@create');
 
   Route::get('profile/edit', 'Admin\ProfileController@edit');
   Route::post('profile/edit', 'Admin\ProfileController@update');
 
+  Route::get('profile', 'Admin\ProfileController@index');
   Route::get('profile/delete', 'Admin\ProfileController@delete');
 
-  Route::get('news', 'Admin\NewsController@index');
-  Route::get('profile', 'Admin\ProfileController@index');
 
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
+//一般閲覧者側
 Route::get('/', 'NewsController@index');
 Route::get('/profile', 'ProfileController@index');
